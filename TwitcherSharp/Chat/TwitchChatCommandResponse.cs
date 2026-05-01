2b7d1c347e57fddbad010d05521dc28df526cdd0
Temptica <nic.vanruy@gmail.com>
2026-05-01 22:38:53 +0200

    Fix #19: Update GetUserCooldown method and create TwitchChatCommandResponse class

---
+++ TwitcherSharp/Chat/TwitchChatCommandResponse.cs
@@ -0,0 +1,27 @@
+using Godot;
+using TwitcherSharp.Interfaces;
+
+namespace TwitcherSharp.Chat;
+
+public partial class TwitchChatCommandResponse: RefCounted, ITwitcherSharp<TwitchChatCommandResponse>
+{
+    public string ResponseMessage { get; set; }
+    public bool UseBot { get; set; } = true;
+    public static TwitchChatCommandResponse FromObject(GodotObject data)
+    {
+        if (data == null) return null;
+        var response = new TwitchChatCommandResponse();
+        response.ResponseMessage = data.Get("respond_message").AsString();
+        response.UseBot = data.Get("use_bot").AsBool();
+        return response;
+    }
+
+    public GodotObject ToGodotObject()
+    {
+        var script = GD.Load<GDScript>("res://addons/twitcher/chat/twitch_chat_command_respond.gd");
+        var instance = script.New().AsGodotObject();
+        instance.Set("respond_message", ResponseMessage);
+        instance.Set("use_bot", UseBot);
+        return instance;
+    }
+}
